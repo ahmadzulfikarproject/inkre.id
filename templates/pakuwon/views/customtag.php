e5e3d4f54605afeafc977da3d87348f152f242ff
fikarcare4u <fikarcare4u@gmail.com>
2021-01-13 09:42:23 +0700

    custom tag

---
+++ templates/pakuwon/views/customtag.php
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V781RJYNG4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V781RJYNG4');
+</script>
