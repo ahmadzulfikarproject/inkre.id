0ff5de13483437f1884aa12eac4c5f541bfd8645
fikarcare4u <fikarcare4u@gmail.com>
2021-06-02 22:26:27 +0700

    update maps

--- templates/pakuwon/views/view_contact.php
+++ templates/pakuwon/views/view_contact.php
@@ -189,7 +189,7 @@
     </div>
   </div>
 </section>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7934.393549193167!2d106.9740925336749!3d-6.104184053259832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMDYnMTUuMiJTIDEwNsKwNTgnNDMuMiJF!5e0!3m2!1sid!2sid!4v1622003350428!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9435.63349384861!2d106.97290952891974!3d-6.104398003045216!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a2129b9fcbfd1%3A0x12439b1f325d58c!2sCV.%20Rehils%20Putra%20Mandiri!5e0!3m2!1sid!2sid!4v1622647403328!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 <div id="contactModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
     <div class="modal-content">
