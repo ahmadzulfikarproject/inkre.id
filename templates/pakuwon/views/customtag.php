81b1c882e1bc98e93648a157a6efc494d80c8d7f
fikarcare4u <fikarcare4u@gmail.com>
2021-06-07 08:47:06 +0700

    fikx tag manager

--- templates/pakuwon/views/customtag.php
+++ templates/pakuwon/views/customtag.php
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-K6J0LFQK4J"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8C8DVEYNZP"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-K6J0LFQK4J');
+  gtag('config', 'G-8C8DVEYNZP');
 </script>
